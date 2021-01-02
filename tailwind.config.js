b149274c2a4271f6989cb92ba736fa20cd65bbb8
Bram Ceulemans <bramceulemans@me.com>
2021-01-03 00:53:51 +0100

    Add purging for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    purge: false,
+    purge: {
+        enabled: process.env.NODE_ENV === 'production',
+        content: ['./src/**/*.html'],
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
